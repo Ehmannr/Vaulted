23eaece5b570e645678fc06b1f5663a3730a5632
Ehmannr <ehmannr@etown.edu>
2021-04-21 21:24:30 -0400

    Changed to sqlite3 login form

--- sqlite3DB.php
+++ sqlite3DB.php
@@ -4,30 +4,62 @@
             $this->open($DBName);
         }
     }
-
+// DELETE FROM Users WHERE Usernames like "_";
     $action = $_POST['action'];
 
     try{
-        $db = new MyDB('test.db');
+        $db = new MyDB('Main.db');
         if($action === 'register'){
-            registerAccount($db, $_POST['username'], $_POST['password']);
+            registerAccount($db, $_POST['username'], hashPass($_POST['password']));
         }
+        elseif($action === 'login'){
+            login($db, $_POST['username'], $_POST['password']);
+        }
+
     } catch(Exception $ex){
         echo $db->lastErrorMsg();
     } finally{
         $db->close();
     }
    
-    function registerAccount($db, string $username, string $password){        
-        $stmt = $db->prepare('INSERT INTO users values (?, ?)');
-        $stmt->bindParam(1, $username);
+
+    function hashPass(string $password){
+        return password_hash($_POST["password"], PASSWORD_BCRYPT);
+    }
+
+
+
+    function registerAccount($db, string $username, string $password){  
+         $stmt = $db->prepare('INSERT INTO Users values (?, ?)');
+         $stmt->bindParam(1, $username);
         $stmt->bindParam(2, $password);
-        $stmt->execute();
+         $stmt->execute();
 
         if($stmt){
-            echo "Table updated successfully";
-        } else{
+            header("Location: index.html");
+         } else{
             echo $stmt->lastErrorMsg();
         }
     }
+    function login($db, string $username, string $password){
+        //get username and password and compare from db
+        try{
+          $passwordQuery = $db->prepare("SELECT Passwords FROM Users WHERE Usernames = ?");
+          $passwordQuery->bindParam(1, $username);
+          $data =$passwordQuery->execute();
+          $row = $data->fetchArray(SQLITE3_ASSOC);
+         // echo($row."hello");
+         // echo($row["Passwords"]." ".$password);
+          
+           if(password_verify($password,$row["Passwords"])){
+            header("Location: tableGui.html");  				
+           }
+           else{ 
+             echo("Username or password is incorrect");        
+           }
+        }catch(PDOException $ex){
+            echo("Username or password is incorrect 123");    
+        }
+      }
+      
 ?>
